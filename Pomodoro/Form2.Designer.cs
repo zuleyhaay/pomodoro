7218b02cd60c048a01d565d1e4db80d811d82627
Züleyha <zuleyhayy@hotmail.com>
2018-12-26 19:47:45 +0300

    project files

---
+++ Pomodoro/Form2.Designer.cs
@@ -0,0 +1,140 @@
+﻿namespace Pomodoro
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form2));
+            this.Stop = new System.Windows.Forms.Button();
+            this.Start = new System.Windows.Forms.Button();
+            this.countdown = new CircularProgressBar.CircularProgressBar();
+            this.timer = new System.Windows.Forms.Timer(this.components);
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.Pomodoro = new System.Windows.Forms.NotifyIcon(this.components);
+            this.time = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // Stop
+            // 
+            resources.ApplyResources(this.Stop, "Stop");
+            this.Stop.Name = "Stop";
+            this.Stop.UseVisualStyleBackColor = true;
+            this.Stop.Click += new System.EventHandler(this.Stop_Click);
+            // 
+            // Start
+            // 
+            resources.ApplyResources(this.Start, "Start");
+            this.Start.Name = "Start";
+            this.Start.UseVisualStyleBackColor = true;
+            this.Start.Click += new System.EventHandler(this.Start_Click);
+            // 
+            // countdown
+            // 
+            this.countdown.AnimationFunction = WinFormAnimation.KnownAnimationFunctions.Liner;
+            this.countdown.AnimationSpeed = 500;
+            this.countdown.BackColor = System.Drawing.Color.Transparent;
+            resources.ApplyResources(this.countdown, "countdown");
+            this.countdown.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.countdown.InnerColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.countdown.InnerMargin = 2;
+            this.countdown.InnerWidth = -1;
+            this.countdown.MarqueeAnimationSpeed = 2000;
+            this.countdown.Maximum = 2000;
+            this.countdown.Name = "countdown";
+            this.countdown.OuterColor = System.Drawing.Color.Gray;
+            this.countdown.OuterMargin = -25;
+            this.countdown.OuterWidth = 26;
+            this.countdown.ProgressColor = System.Drawing.Color.Tomato;
+            this.countdown.ProgressWidth = 25;
+            this.countdown.SecondaryFont = new System.Drawing.Font("Microsoft Sans Serif", 24F);
+            this.countdown.StartAngle = 270;
+            this.countdown.SubscriptColor = System.Drawing.Color.FromArgb(((int)(((byte)(166)))), ((int)(((byte)(166)))), ((int)(((byte)(166)))));
+            this.countdown.SubscriptMargin = new System.Windows.Forms.Padding(10, -35, 0, 0);
+            this.countdown.SubscriptText = "";
+            this.countdown.SuperscriptColor = System.Drawing.Color.FromArgb(((int)(((byte)(166)))), ((int)(((byte)(166)))), ((int)(((byte)(166)))));
+            this.countdown.SuperscriptMargin = new System.Windows.Forms.Padding(10, 35, 0, 0);
+            this.countdown.SuperscriptText = "";
+            this.countdown.TextMargin = new System.Windows.Forms.Padding(8, 8, 0, 0);
+            this.countdown.Value = 68;
+            // 
+            // timer
+            // 
+            this.timer.Interval = 1000;
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            // 
+            // timer2
+            // 
+            this.timer2.Interval = 1000;
+            // 
+            // Pomodoro
+            // 
+            resources.ApplyResources(this.Pomodoro, "Pomodoro");
+            this.Pomodoro.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.Pomodoro_MouseDoubleClick);
+            // 
+            // time
+            // 
+            this.time.BackColor = System.Drawing.SystemColors.Info;
+            resources.ApplyResources(this.time, "time");
+            this.time.Name = "time";
+            // 
+            // Form2
+            // 
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoValidate = System.Windows.Forms.AutoValidate.EnablePreventFocusChange;
+            this.CausesValidation = false;
+            this.Controls.Add(this.time);
+            this.Controls.Add(this.Stop);
+            this.Controls.Add(this.Start);
+            this.Controls.Add(this.countdown);
+            this.DataBindings.Add(new System.Windows.Forms.Binding("Location", global::Pomodoro.Properties.Settings.Default, "Location", true, System.Windows.Forms.DataSourceUpdateMode.OnPropertyChanged));
+            this.DataBindings.Add(new System.Windows.Forms.Binding("MaximumSize", global::Pomodoro.Properties.Settings.Default, "LastLoc", true, System.Windows.Forms.DataSourceUpdateMode.OnPropertyChanged));
+            this.Location = global::Pomodoro.Properties.Settings.Default.Location;
+            this.MaximumSize = global::Pomodoro.Properties.Settings.Default.LastLoc;
+            this.MinimumSize = this.Size;
+            this.Name = "Form2";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private CircularProgressBar.CircularProgressBar countdown;
+        private System.Windows.Forms.Timer timer;
+        private System.Windows.Forms.Button Stop;
+        private System.Windows.Forms.Button Start;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.NotifyIcon Pomodoro;
+        private System.Windows.Forms.TextBox time;
+    }
+}
---
+++ Pomodoro/Form2.cs
@@ -0,0 +1,338 @@
+﻿using System;
+using System.Collections.Specialized;
+using System.Configuration;
+using System.Drawing;
+using System.IO;
+using System.Net;
+using System.Reflection;
+using System.Text;
+using System.Windows.Forms;
+
+
+
+namespace Pomodoro
+{
+    public partial class Form2 : Form
+    {
+        private WebClient _client = null;
+        private DateTime currentTime;
+        private int pomodoroTimes;
+        private static int _workTime;
+        private int _shortBreak;
+        private int _longBreak;
+        private int _checkMarks;
+        private char _twoDots = ':';
+        private int _value;
+        private static int _seconds;
+
+        public Form2()
+        {
+            InitializeComponent();
+            SetEvent(OnTimer, timer);
+            SetEvent(ShortTimer, timer1);
+            SetEvent(LongTimer, timer2);
+            countdown.Value = 0;
+            MessageBallons(1);
+            MaximizeBox = false;
+            Stop.Visible = false;
+        }
+
+        private void InitialValues()
+        {
+            _workTime = 25;
+            _shortBreak = 5;
+            _longBreak = 15;
+            _checkMarks = 0;
+            _value = 0;
+            pomodoroTimes = 0;
+            _seconds = 60;
+        }
+
+        private void Start_Click(object sender, EventArgs e)
+        {
+            InitialValues();
+            Stop.Visible = true;
+            Start.Enabled = false;
+            SetPresence("away");
+            SetProgressValue(_workTime);
+
+            timer.Start();
+
+            MinimizeForm();
+            MessageBallons(_workTime);
+        }
+
+        private void SetPresence(string status)
+        {
+            _client = new WebClient();
+            var appSettings = ConfigurationManager.AppSettings;
+            var key = appSettings.GetKey(0);
+            _client.UploadValues("https://polirock.slack.com/api/users.setPresence", "POST",
+                new NameValueCollection()
+                {
+                    {"token", appSettings[key]},
+                    {"presence", status}
+                });
+        }
+
+        private void OnTimer(object sender, EventArgs e)
+        {
+            _seconds--;
+
+            if (_workTime == 0)
+            {
+
+                _workTime = 25;
+
+                SetPresence("auto");
+                _checkMarks++;
+                timer.Stop();
+
+                if (_checkMarks < 4)
+                {
+                    SetProgressValue(_shortBreak);
+                    timer1.Start();
+
+                    MaximizeForm();
+                    MessageBallons(_shortBreak);
+                }
+
+                else
+                {
+                    pomodoroTimes++;
+                    SetProgressValue(_longBreak);
+                    _checkMarks = 0;                    
+                    timer2.Start();
+
+                    MaximizeForm();
+                    MessageBallons(_longBreak);
+                }
+
+                return;
+            }
+
+            WriteTimetoText(_workTime - 1, _seconds);
+
+            if (_seconds == 0)
+            {
+                _workTime--;
+                WriteTimetoText(_workTime, _seconds);
+                _seconds = 60;
+            }
+
+            countdown.Value += _value;
+            countdown.Update();
+        }
+
+        private void ShortTimer(object sender, EventArgs e)
+        {
+            _seconds--;
+
+            countdown.Value += _value;
+            countdown.Update();
+
+            if (_shortBreak == 0)
+            {
+                timer1.Stop();
+                _shortBreak = 5;
+
+                SetPresence("away");
+
+                SetProgressValue(_workTime);
+                timer.Start();
+
+                MinimizeForm();
+                MessageBallons(_workTime);
+
+                return;
+            }
+
+            WriteTimetoText(_shortBreak - 1, _seconds);
+
+            if (_seconds == 0)
+            {
+                _shortBreak--;
+                WriteTimetoText(_shortBreak, _seconds);
+                _seconds = 60;
+            }
+        }
+
+        private void LongTimer(object sender, EventArgs e)
+        {
+            _seconds--;
+
+
+            countdown.Value += _value;
+            countdown.Update();
+
+            if (_longBreak == 0)
+            {
+                timer2.Stop();
+                _longBreak = 15;
+
+                SetPresence("away");
+
+                SetProgressValue(_workTime);
+                timer.Start();
+
+                MinimizeForm();
+                MessageBallons(_workTime);
+
+                timer.Start();
+
+                return;
+            }
+
+            WriteTimetoText(_longBreak - 1, _seconds);
+
+            if (_seconds == 0)
+            {
+                _longBreak--;
+                WriteTimetoText(_longBreak, _seconds);
+                _seconds = 60;
+            }
+        }
+
+        private void SetProgressValue(int val)
+        {
+            countdown.Value = 0;
+            countdown.Minimum = 0;
+            countdown.Maximum = 2000;
+            _value = (2000 / (60 * val));
+
+            if (val == _workTime)
+            {
+                countdown.ProgressColor = System.Drawing.Color.Tomato;
+            }
+            else
+            {
+                countdown.ProgressColor = System.Drawing.Color.Green;
+            }
+        }
+
+        private void MinimizeForm()
+        {
+            this.WindowState = FormWindowState.Minimized;
+            this.ShowInTaskbar = false;
+            Pomodoro.Visible = true;
+        }
+
+        private void MaximizeForm()
+        {
+            this.WindowState = FormWindowState.Normal;
+            this.ShowInTaskbar = true;
+            Pomodoro.Visible = true;
+        }
+
+        private void WriteTimetoText(int val, int sec)
+        {
+            time.Text = val.ToString() + _twoDots.ToString() + sec.ToString();
+        }
+
+        private void WriteToFileTimes()
+        {
+            currentTime = DateTime.Now;
+            StringBuilder sb = new StringBuilder();
+            string path = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+            sb.Append(currentTime + "   " + pomodoroTimes + "  pomodoros completed.");
+            File.AppendAllText(path + ".log", sb.ToString() + Environment.NewLine);
+        }
+
+        private void MessageBallons(int val)
+        {
+            if (val == _workTime)
+            {
+                Pomodoro.BalloonTipText = "Süreniz başladı! Kolay gelsin. 🤓";
+                Pomodoro.ShowBalloonTip(1000);
+            }
+            else if (val == _shortBreak)
+            {
+                Pomodoro.BalloonTipText = "Kahvelerimizi yenilesek mi? ☕";
+                Pomodoro.ShowBalloonTip(1000);
+            }
+            else if (val == _longBreak)
+            {
+                Pomodoro.BalloonTipText = "Çok çalıştık bunu hak ettik. 15 dk sonra görüşürüz! ☺️";
+                Pomodoro.ShowBalloonTip(1000);
+            }
+            else if (val == 0)
+            {
+                if (pomodoroTimes >= 3)
+                {
+                    Pomodoro.BalloonTipText = "Bugün " + pomodoroTimes + " pomodoro uygulamışsın 😎 Harikasın! Tekrar görüşürüz 👋🏽";
+                    Pomodoro.ShowBalloonTip(1000);
+                }
+                else
+                {
+                    Pomodoro.BalloonTipText = "Bugün " + pomodoroTimes + " pomodoro uygulamışsın 🤔 Pomodoroyu daha fazla kullanarak çalışma akışının verimini arttırmalısın. Tekrar görüşürüz 👋🏽";
+                    Pomodoro.ShowBalloonTip(1000);
+                }
+
+            }
+            else if (val == 1)
+            {
+                Pomodoro.BalloonTipText = "Pomodoro'ya hoşgeldin! ☺️";
+                Pomodoro.ShowBalloonTip(1000);
+            }
+        }
+
+        private void SetEvent(EventHandler handler, Timer time)
+        {
+            time.Tick += new EventHandler(handler);
+            time.Interval = 1000;
+        }
+
+
+        private void Stop_Click(object sender, EventArgs e)
+        {
+            SetPresence("auto");
+            timer.Dispose();
+            timer1.Dispose();
+            timer2.Dispose();
+            Start.Enabled = true;
+            Stop.Visible = false;
+            MessageBallons(0);
+            WriteTimetoText(0, 0);
+            WriteToFileTimes();
+        }
+
+        private void Pomodoro_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            MaximizeForm();
+        }
+
+        protected override void OnFormClosing(FormClosingEventArgs e)
+        {
+            if (e.CloseReason == CloseReason.UserClosing)
+            {
+                DialogResult close = new DialogResult();
+                close = MessageBox.Show("Emin misiniz?", "Çıkış",
+                    MessageBoxButtons.YesNo,
+                    MessageBoxIcon.Warning,
+                    MessageBoxDefaultButton.Button2);
+
+                if (close == DialogResult.Yes)
+                {
+                    this.Visible = false;
+                    SetPresence("auto");
+                    timer.Dispose();
+                    timer1.Dispose();
+                    timer2.Dispose();
+                    WriteToFileTimes();
+                    e.Cancel = false;
+                }
+
+                else
+                {
+                    e.Cancel = true;
+                }
+            }
+
+            base.OnFormClosing(e);
+        }
+
+    }
+
+
+
+}
